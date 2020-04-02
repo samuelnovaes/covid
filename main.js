8d50a13d2fc04e7dea5b58a62d3cad9adf30c6f7
Samuel Novaes <samuel.novaes.96@gmail.com>
2020-04-02 19:53:27 -0300

    .

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const loader = document.getElementById('loader');
 const error = document.getElementById('error');
 const header = document.getElementsByTagName('header')[0];
+const container = document.getElementById('charts');
+const checkbox = document.getElementById('extender');
+const charts = [];
 
-const populate = (title, data, fn) => {
-	const daysInBrazil = data.Brazil.filter((x) => x.confirmed > 0).length;
-	const labels = Array(daysInBrazil).fill(0).map((x, i) => `${i + 1}º dia`);
-	const countBrazil = data.Brazil.filter((x) => x.confirmed > 0).map(fn);
-	const countItaly = data.Italy.filter((x) => x.confirmed > 0).map(fn).filter((x, i) => i < daysInBrazil);
+const populate = (title, data, extend, fn) => {
+	const days = data[extend ? 'Italy' : 'Brazil'].filter((x) => x.confirmed > 0).length;
+	const labels = Array(days).fill(0).map((x, i) => `${i + 1}º dia`);
+	const countBrazil = data.Brazil.filter((x) => x.confirmed > 0).map(fn).filter((x, i) => i < days);
+	const countItaly = data.Italy.filter((x) => x.confirmed > 0).map(fn).filter((x, i) => i < days);
 	const element = document.createElement('canvas');
-	
-	document.body.appendChild(element);
-
-	new Chart(element, {
+	container.appendChild(element);
+	charts.push(new Chart(element, {
 		type: 'line',
 		data: {
 			labels,
@@ -48,21 +49,31 @@ const populate = (title, data, fn) => {
 				displayColors: false
 			}
 		}
-	});
+	}));
 };
+
+const populateAll = (data, extend = false) => {
+	for (const chart of charts) {
+		chart.destroy();
+	}
+	charts.length = 0;
+	container.innerHTML = '';
+	populate('Casos totais', data, extend, (x) => x.confirmed);
+	populate('Casos diários', data, extend, (x, i, arr) => arr[i - 1] ? x.confirmed - arr[i - 1].confirmed : x.confirmed);
+	populate('Mortes totais', data, extend, (x) => x.deaths);
+	populate('Mortes diárias', data, extend, (x, i, arr) => arr[i - 1] ? x.deaths - arr[i - 1].deaths : x.deaths);
+	populate('Casos recuperados', data, extend, (x) => x.recovered);
+	populate('Casos recuperados diários', data, extend, (x, i, arr) => arr[i - 1] ? x.recovered - arr[i - 1].recovered : x.recovered);
+	populate('Casos ativos', data, extend, (x) => x.confirmed - x.recovered - x.deaths);
+}
 
 (async () => {
 	try {
 		const data = await (await fetch('https://pomber.github.io/covid19/timeseries.json')).json();
-
-		populate('Casos totais', data, (x) => x.confirmed);
-		populate('Casos diários', data, (x, i, arr) => arr[i - 1] ? x.confirmed - arr[i - 1].confirmed : x.confirmed);
-		populate('Mortes totais', data, (x) => x.deaths);
-		populate('Mortes diárias', data, (x, i, arr) => arr[i - 1] ? x.deaths - arr[i - 1].deaths : x.deaths);
-		populate('Casos recuperados', data, (x) => x.recovered);
-		populate('Casos recuperados diários', data, (x, i, arr) => arr[i - 1] ? x.recovered - arr[i - 1].recovered : x.recovered);
-		populate('Casos ativos', data, (x) => x.confirmed - x.recovered - x.deaths);
-
+		populateAll(data);
+		checkbox.onchange = (e) => {
+			populateAll(data, e.target.checked);
+		};
 		loader.style.display = 'none';
 		header.style.display = 'block';
 	}
